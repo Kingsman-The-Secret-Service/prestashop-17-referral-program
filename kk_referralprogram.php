11ad79db7aab1ff6082fab92aa9dec1b4697ddb6
Kaviarasan K K <kaviarasankk@gmail.com>
2017-06-26 17:14:59 +0530

    initial release

---
+++ kk_referralprogram.php
@@ -0,0 +1,201 @@
+<?php
+/*
+* 2007-2015 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2015 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Kk_ReferralProgram extends Module
+{
+    private $templateFile;
+
+    public function __construct()
+    {
+        $this->name = 'kk_referralprogram';
+        $this->author = 'Kaviarasan K K';
+        $this->version = '1.0.0';
+        $this->need_instance = 0;
+
+        $this->ps_versions_compliancy = [
+            'min' => '1.7.0.0',
+            'max' => _PS_VERSION_,
+        ];
+
+        $this->bootstrap = true;
+        parent::__construct();
+
+        $this->displayName = $this->trans('Referral Program', array(), 'Modules.ReferralProgram.Admin');
+        $this->description = $this->trans('Synchornize newsletter emails to mailchimp.', array(), 'Modules.ReferralProgram.Admin');
+    }
+
+    public function install()
+    {
+        return parent::install()
+            && $this->registerHook('displayMyAccountBlock')
+            && $this->registerHook('displayCustomerAccount');
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+
+    public function getContent()
+    {
+        $output = '';
+        $errors = array();
+
+        if (Tools::isSubmit('submitHomeFeatured')) {
+
+            $nbr = Tools::getValue('HOME_FEATURED_NBR');
+            if (!Validate::isInt($nbr) || $nbr <= 0) {
+                $errors[] = $this->trans('The number of products is invalid. Please enter a positive number.', array(), 'Modules.MailchimpSync.Admin');
+            }
+
+            $cat = Tools::getValue('HOME_FEATURED_CATERGORY');
+
+            if (count($cat) <= 0) {
+                $errors[] = $this->trans('The category ID is invalid. Please choose an existing category ID.', array(), 'Modules.MailchimpSync.Admin');
+            }
+
+            $rand = Tools::getValue('HOME_FEATURED_RANDOMIZE');
+            if (!Validate::isBool($rand)) {
+                $errors[] = $this->trans('Invalid value for the "randomize" flag.', array(), 'Modules.MailchimpSync.Admin');
+            }
+            if (isset($errors) && count($errors)) {
+                $output = $this->displayError(implode('<br />', $errors));
+            } else {
+                Configuration::updateValue('HOME_FEATURED_NBR', (int) $nbr);
+                Configuration::updateValue('HOME_FEATURED_CAT',  implode(';', $cat));
+                Configuration::updateValue('HOME_FEATURED_RANDOMIZE', (bool) $rand);
+
+                $this->_clearCache('*');
+
+                $output = $this->displayConfirmation($this->trans('The settings have been updated.', array(), 'Admin.Notifications.Success'));
+            }
+        }
+
+        return $output.$this->renderForm();
+    }
+
+    public function renderForm()
+    {
+
+        $fields_form = array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->trans('Settings', array(), 'Admin.Global'),
+                    'icon' => 'icon-cogs',
+                ),
+
+                'description' => $this->trans('To add products to your homepage, simply add them to the corresponding product category (default: "Home").', array(), 'Modules.MailchimpSync.Admin'),
+                'input' => array(
+                    array(
+                        'type' => 'text',
+                        'label' => $this->trans('Number of products to be displayed', array(), 'Modules.MailchimpSync.Admin'),
+                        'name' => 'HOME_FEATURED_NBR',
+                        'class' => 'fixed-width-xs',
+                        'desc' => $this->trans('Set the number of products that you would like to display on homepage (default: 8).', array(), 'Modules.MailchimpSync.Admin'),
+                    ),
+                    array(
+                        'type' => 'select',
+                        'label' => $this->trans('Manufacturers'),
+                        'name' => 'HOME_FEATURED_CATERGORY[]',
+                        'multiple' => true,
+                        'class' => 'fixed-width-xs',
+                        'options' => array(
+                            'query' => $categories,
+                            'id' => 'id_category',
+                            'name' => 'name'
+                        )
+                    ),
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->trans('Randomly display featured products', array(), 'Modules.MailchimpSync.Admin'),
+                        'name' => 'HOME_FEATURED_RANDOMIZE',
+                        'class' => 'fixed-width-xs',
+                        'desc' => $this->trans('Enable if you wish the products to be displayed randomly (default: no).', array(), 'Modules.MailchimpSync.Admin'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => 1,
+                                'label' => $this->trans('Yes', array(), 'Admin.Global'),
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => 0,
+                                'label' => $this->trans('No', array(), 'Admin.Global'),
+                            ),
+                        ),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->trans('Save', array(), 'Admin.Actions'),
+                ),
+            ),
+        );
+
+        $lang = new Language((int) Configuration::get('PS_LANG_DEFAULT'));
+
+        $helper = new HelperForm();
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->default_form_language = $lang->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
+        $helper->id = (int) Tools::getValue('id_carrier');
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitHomeFeatured';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFieldsValues(),
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($fields_form));
+    }
+
+    public function getConfigFieldsValues()
+    {
+        return array(
+            'HOME_FEATURED_NBR' => Tools::getValue('HOME_FEATURED_NBR', (int) Configuration::get('HOME_FEATURED_NBR')),
+            'HOME_FEATURED_CATERGORY[]' => Tools::getValue('HOME_FEATURED_CATERGORY[]', explode(';', Configuration::get('HOME_FEATURED_CAT'))),
+            'HOME_FEATURED_RANDOMIZE' => Tools::getValue('HOME_FEATURED_RANDOMIZE', (bool) Configuration::get('HOME_FEATURED_RANDOMIZE')),
+        );
+    }
+
+    public function hookDisplayMyAccountBlock($param){
+
+        return $this->fetch('module:/views/templates/front/referrallinks.tpl');
+    }
+
+    public function hookDisplayCustomerAccount($param){
+
+        return $this->fetch('module:/views/templates/front/referralblocks.tpl');
+    }
+    
+}
---
+++ views/templates/front/referralblocks.tpl
@@ -0,0 +1,10 @@
+<li>
+<a href="#" title="Title" rel="nofollow">Referral Program</a>
+</li>
+
+<a class="col-lg-4 col-md-6 col-sm-6 col-xs-12" id="returns-link" href="{$urls.pages.order_follow}">
+          <span class="link-item">
+            <i class="material-icons">&#xE860;</i>
+            {l s='Merchandise returns' d='Shop.Theme.CustomerAccount'}
+          </span>
+        </a>
